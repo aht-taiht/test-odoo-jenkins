94203c73c282b93021aab6ed891be5b56b3210c8
Huyhoclaptrinh <giahuyphung282@gmail.com>
2023-10-25 09:14:29 +0700

    [FIX][MAVND2301SCP-8]Chỉnh sửa lại địa chỉ giao hàng

---
+++ addons/custom/onnet_sale_order/models/sale_order.py
@@ -0,0 +1,22 @@
+from odoo import _, fields, models
+from odoo.odoo import api
+
+LOCKED_FIELD_STATES = {
+    state: [('readonly', True)]
+    for state in {'done', 'cancel'}
+}
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    address = fields.Char(string="Delivery Address",
+                          compute='_compute_address',
+                          store=True, readonly=False, required=True, precompute=True,
+                          states=LOCKED_FIELD_STATES,
+                          domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]", )
+
+    @api.depends('partner_id')
+    def _compute_address(self):
+        for order in self:
+            order.address = order.partner_id.display_name if order.partner_id else False
